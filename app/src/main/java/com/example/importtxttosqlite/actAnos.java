07b608812bc38e9e7fe734e32a4ce18cf561502a
Paulo Nascimento <paulomnasc@gmail.com>
2023-04-01 21:02:40 -0300

    Mudei para nova API Key de Paulo e add a OpenAI-Organization no header do request. Vide dúvidas de uso de API em https://platform.openai.com/account/usage

--- app/src/main/java/com/example/importtxttosqlite/actAnos.java
+++ app/src/main/java/com/example/importtxttosqlite/actAnos.java
@@ -3,6 +3,7 @@ package com.example.importtxttosqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.widget.ListView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class actAnos extends AppCompatActivity {
@@ -37,12 +42,37 @@ public class actAnos extends AppCompatActivity {
 
     private Integer idSelecionado;
 
+    private void copyDatabase() throws IOException {
+        InputStream inputStream = getApplicationContext().getAssets().open("nome_do_banco_de_dados.sqlite");
+        String outFileName = getDatabasePath("StudyApp").getPath();
+        OutputStream outputStream = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_anos);
+
+
+        SharedPreferences preferences = getSharedPreferences("my_preferences", MODE_PRIVATE);
+        if (preferences.getBoolean("is_first_run", true)) {
+            // Coloque aqui o código que deve ser executado somente na primeira vez que o aplicativo é iniciado
+            // Por exemplo, você pode mostrar um tutorial ou exibir uma mensagem de boas-vindas para o usuário
+            preferences.edit().putBoolean("is_first_run", false).apply();
+
+
+
+        }
+
 
 
         listaAnos = (ListView) findViewById(R.id.lstAnos);
